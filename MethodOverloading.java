9323546fe85f35089b71807e00c85711939fc19a
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-12 22:26:26 +0530

    Add files via upload

---
+++ MethodOverloading.java
@@ -0,0 +1,23 @@
+package Day9;
+class Calculation{
+	int add(int a, int b) {
+		return a+b;
+	}
+	int add(int a, int b, int c) {
+		return a+b+c;
+	}
+	double add(double a, double b) {
+		return a+b;
+	}
+}
+public class MethodOverloading {
+
+	public static void main(String[] args) {
+		Calculation c= new Calculation();
+		System.out.println(c.add(10, 20));
+		System.out.println(c.add(10, 20, 30));
+		System.out.println(c.add(10.5,20.5));
+
+	}
+
+}
