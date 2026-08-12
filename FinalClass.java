cda64c280c8a0239c3d38d31110be972f58dbeb6
Dhanya Anbukkarasu <146648747+Dhanya2006@users.noreply.github.com>
2026-08-12 22:25:29 +0530

    Add files via upload

---
+++ FinalClass.java
@@ -0,0 +1,15 @@
+package Day9;
+final class Animal{
+	void eat() {
+		System.out.println("animal  is eating");
+	}
+}
+public class FinalClass {
+
+	public static void main(String[] args) {
+		Animal a = new Animal();
+		a.eat();
+
+	}
+
+}
